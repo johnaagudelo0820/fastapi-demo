c592a3ed3b9affbb38831e8975316136cbc9b382
John Agudelo <johnaagudelo@gmail.com>
2025-11-24 22:11:24 -0500

    feat: Add function to create all database tables and update FastAPI imports

--- db.py
+++ db.py
@@ -1,12 +1,15 @@
-from sqlmodel import Session, create_engine
+from sqlmodel import Session, create_engine, SQLModel
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, FastAPI
 
 SQLLITE_NAME = "db.sqlite3"
 SQLITE_URL = f"sqlite:///{SQLLITE_NAME}"
 
-
 engine = create_engine(SQLITE_URL)
+
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
 
 def get_session():
     with Session(engine) as session:
